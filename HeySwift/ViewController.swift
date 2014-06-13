03fa5bee71ca115bca4abd6abc6f16b7b929bcab
khalid <mustkre8@gmail.com>
2014-06-12 21:29:00 -0400

    Initial Commit

---
+++ HeySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeySwift
+//
+//  Created by k on 6/12/14.
+//  Copyright (c) 2014 iksnae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
