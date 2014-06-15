cac4476f5a42c0efcb872ff6c38410d88113f0d5
khalid <mustkre8@gmail.com>
2014-06-15 09:18:01 -0400

    refactored ViewController to be more reactive

--- HeySwift/ViewController.swift
+++ HeySwift/ViewController.swift
@@ -18,70 +18,67 @@ class ViewController: UIViewController, CVDatastoreDelegate, UICollectionViewDel
     
     @IBOutlet var collectionView : UICollectionView
     
-    
-    
-    // Properties
+    // Properties ( Constants )
     
     let datasource:CVDatastore = CVDatastore()
     let activityView:UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
-    let searchBar:UISearchBar = UISearchBar()
-    
-    var selectedIndexPath:NSIndexPath = NSIndexPath(forItem: 0, inSection: 0)
     let blurEffectStyle = UIBlurEffectStyle.Dark
     
     
     
-    // ViewController Overrides
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: activityView)
-        
-        self.view.backgroundColor = UIColor.blackColor()
-        self.collectionView.backgroundColor = UIColor.clearColor()
-        self.collectionView.dataSource = self.datasource
-        self.collectionView.delegate = self
-        self.datasource.delegate = self
-        
-        self.collectionView.contentOffset = CGPoint(x: 0, y: 44);
-        
-        self.searchBar.frame = CGRect(x: 0, y: 0, width:  CGRectGetWidth(self.collectionView.frame), height: 44)
-        self.searchBar.searchBarStyle = UISearchBarStyle.Minimal
-        self.searchBar.barStyle = UIBarStyle.Black
-        self.searchBar.autocorrectionType = UITextAutocorrectionType.No
-        self.searchBar.delegate = self
-        self.navigationController.navigationBar.addSubview(self.searchBar)
-        
-        self.datasource.searchForImages("frida", start: 0);
-    }
+    // Properties ( Reactive )
     
-    
-    
-    // DataSource Delegate Methods
-    
-    func dataSourceDidChangeState(newState: DataSourceState)
-    {
-        switch(newState){
-        case .Loaded, .Failed, .Idle:
-            println("unhandled valid state:\t\t" + newState.toRaw())
+    var selectedIndexPath:NSIndexPath = NSIndexPath(forItem: 0, inSection: 0){
+        didSet{
+            let entry:ResultsEntry = self.datasource.items.objectAtIndex(self.selectedIndexPath.row) as ResultsEntry
             
-        case .Loading:
-            self.activityView.startAnimating()
-
-        case .Ready:
-            
-            dispatch_async(dispatch_get_main_queue(), {
-                let count = self.datasource.collectionView(self.collectionView, numberOfItemsInSection: 0)
-                
-                self.title = "\(count) images for '" + self.datasource.searchQuery + "'"
-                self.collectionView.reloadData()
-                self.activityView.stopAnimating()
-            })
-        default:
-            println("unhandled invalid state:" + newState.toRaw())
+            self.navigationController.presentViewController(self.photoVC, animated: true, completion:{
+                self.photoVC.entry = entry
+                })
         }
     }
+    
+    var totalResults:Int = 0
+    {
+        didSet{
+            self.title = "\(totalResults) images for '" + self.datasource.searchQuery + "'"
+            
+        }
+    }
+    
+    var isSearching:Bool = false
+    {
+        didSet{
+            if(isSearching)
+            {
+                self.title = ""
+                self.view.addSubview(self.blurView)
+                self.view.addSubview(self.dismissSearchButton);
+            }
+            else
+            {
+                searchBar.resignFirstResponder()
+                self.title = "Total Images: \(self.datasource.collectionView(self.collectionView, numberOfItemsInSection: 0))"
+                self.blurView.removeFromSuperview()
+                self.dismissSearchButton.removeFromSuperview()
+                searchBar.text = ""
+            }
+        }
+    }
+    
+    // Properties ( Lazy Instantiated )
+    
+    @lazy var searchBar:UISearchBar = {
+        
+        let sb = UISearchBar()
+        sb.frame = CGRect(x: 0, y: 0, width:  CGRectGetWidth(self.collectionView.frame), height: 44)
+        sb.searchBarStyle = UISearchBarStyle.Minimal
+        sb.barStyle = UIBarStyle.Black
+        sb.autocorrectionType = UITextAutocorrectionType.No
+        sb.delegate = self
+        return sb
+    }()
     
     @lazy var photoVC:PhotoViewController =
     {
@@ -100,55 +97,99 @@ class ViewController: UIViewController, CVDatastoreDelegate, UICollectionViewDel
         let view = UIVisualEffectView(effect: blurEffect)
         view.frame = self.collectionView.bounds
         return view
-        }()
+    }()
     
     
-    func collectionView(collectionView: UICollectionView!, didSelectItemAtIndexPath indexPath: NSIndexPath!)
-    {
-        self.selectedIndexPath = indexPath
-        let entry:ResultsEntry = self.datasource.items.objectAtIndex(self.selectedIndexPath.row) as ResultsEntry
-        
-   
-        
-        self.navigationController.presentViewController(self.photoVC, animated: true, completion:{
-            self.photoVC.entry = entry
-            
-            })
-    }
-    
-    
+    // Functions
     func onDismissSearch()
     {
         self.searchBar.resignFirstResponder()
     }
     
+    
+    
+    // ViewController Overrides
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: activityView)
+        self.view.backgroundColor = UIColor.blackColor()
+        self.navigationController.navigationBar.addSubview(self.searchBar)
+        
+        
+        self.collectionView.backgroundColor = UIColor.clearColor()
+        self.collectionView.dataSource = self.datasource
+        self.collectionView.delegate = self
+        self.datasource.delegate = self
+        self.collectionView.contentOffset = CGPoint(x: 0, y: 44);
+        
+        self.datasource.searchForImages("frida", start: 0);
+    }
+    
+    
+    
+    
+    
+    // DataSource Delegate Methods
+    
+    func dataSourceDidChangeState(newState: DataSourceState)
+    {
+        switch(newState){
+            
+        case .Loaded, .Failed, .Idle:
+            println("unhandled valid state:\t\t" + newState.toRaw())
+            
+        case .Loading:
+            self.activityView.startAnimating()
+
+        case .Ready:
+            dispatch_async(dispatch_get_main_queue(), {
+                self.totalResults = self.datasource.collectionView(self.collectionView, numberOfItemsInSection: 0)
+                self.collectionView.reloadData()
+                self.activityView.stopAnimating()
+            })
+        default:
+            println("unhandled invalid state:" + newState.toRaw())
+        }
+    }
+    
+    // UICollectionView Delegate Methods
+    
+    func collectionView(collectionView: UICollectionView!, didSelectItemAtIndexPath indexPath: NSIndexPath!)
+    {
+        self.selectedIndexPath = indexPath
+    }
+    
+    
+    
+    // UISearchBar Delegate Methods
+    
     func searchBarTextDidBeginEditing(searchBar: UISearchBar!)
     {
-        self.title = ""
-        self.view.addSubview(self.blurView)
-        self.view.addSubview(self.dismissSearchButton);
+        self.isSearching = true
     }
+    
     func searchBarShouldEndEditing(searchBar: UISearchBar!) -> Bool
     {
-        searchBar.resignFirstResponder()
+        self.isSearching = false;
         return true
     }
+    
     func searchBarTextDidEndEditing(searchBar: UISearchBar!)
     {
-        searchBar.resignFirstResponder()
-        self.title = "Total Images: \(self.datasource.collectionView(self.collectionView, numberOfItemsInSection: 0))"
-        self.blurView.removeFromSuperview()
-        self.dismissSearchButton.removeFromSuperview()
-        searchBar.text = ""
+        self.isSearching = false;
     }
     
     func searchBarSearchButtonClicked(searchBar: UISearchBar!)
     {
         println(self.searchBar.text)
         self.datasource.searchForImages(searchBar.text, start: 0)
-        self.searchBar.resignFirstResponder()
-        
+        self.isSearching = false;
     }
+    
+    
     
     
 }
